41423f2c5e2fad350ffcb661e55fae5a7060b48d
soodax <mirakov.david@mail.ru>
2022-06-15 00:00:03 +0300

    v2.1 /add ru language + some fixes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,69 +14,146 @@ import header_logo from './../../assets/images/header-logo.png';
 
 const Main = (props) => {
 
-    const [weather, setWeather] = useState();
-    const [currentWeather, setCurrentWeather] = useState();
+    const [weather, setWeather] = useState(null);
+    const [currentWeather, setCurrentWeather] = useState(null);
+    const [currentLanguage, setCurrentLanguage] = useState('en');
 
     let currentCity = 'Moscow';
+    // let currentLanguage = 'eng';
     const api = 'd779f17843098d3158c1d2a9115ce239';
     let arr = [];
     let currentDate;
-    
+
+
     const searchCity = () => {
-        
+
         axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&appid=${api}`).then(city => {
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.data[0].lat}&lon=${city.data[0].lon}&appid=${api}&units=metric`).then(response => {
+
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.data[0].lat}&lon=${city.data[0].lon}&appid=${api}&units=metric&lang=${currentLanguage}`).then(response => {
                 setCurrentWeather(response)
             })
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.data[0].lat}&lon=${city.data[0].lon}&appid=${api}&units=metric`).then(response => {
+            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${city.data[0].lat}&lon=${city.data[0].lon}&appid=${api}&units=metric&lang=${currentLanguage}`).then(response => {
                 for (let i = 0; i < 7; i++) {
                     arr[i] = response.data.daily[i]
                 }
                 setWeather(arr)
-                
+
             })
         })
-        
+
     }
-
-
 
     useEffect(() => {
         searchCity(currentCity)
-    }, [currentCity])
+    }, [currentLanguage])
 
     if (!weather) {
         return <div>loading...</div>
     }
 
     if (weather) {
+        if (currentLanguage == 'ru') {
+            for (let i = 0; i < 7; i++) {
+                switch (weather[i].weather[0].main) {
+                    case 'Clouds':
+                        weather[i].weather[0].main = 'Облака';
+                        break;
+                    case 'Clear':
+                        weather[i].weather[0].main = 'Ясно';
+                        break;
+                    case 'Snow':
+                        weather[i].weather[0].main = 'Снег';
+                        break;
+                    case 'Rain':
+                        weather[i].weather[0].main = 'Дождь';
+                        break;
+                    case 'Drizzle':
+                        weather[i].weather[0].main = 'Изморось';
+                        break;
+                    case 'Thunderstorm':
+                        weather[i].weather[0].main = 'Гроза';
+                        break;
+                }
+            }
+        }
         switch (new Date(weather[0].dt * 1000).getDay()) {
             case 1:
-                currentDate = 'Monday';
+                if (currentLanguage == 'en') {
+                    currentDate = 'Monday';
+                } else if (currentLanguage == 'ru') {
+                    currentDate = 'Понедельник';
+                }
+
                 break;
             case 2:
-                currentDate = 'Tuesday';
+
+                if (currentLanguage == 'en') {
+                    currentDate = 'Tuesday';
+                } else if (currentLanguage == 'ru') {
+                    currentDate = 'Вторник';
+                }
                 break;
             case 3:
-                currentDate = 'Wednesday';
+                if (currentLanguage == 'en') {
+                    currentDate = 'Wednesday';
+                } else if (currentLanguage == 'ru') {
+                    currentDate = 'Среда';
+                }
+
                 break;
             case 4:
-                currentDate = 'Thursday';
+                if (currentLanguage == 'en') {
+                    currentDate = 'Thursday';
+                } else if (currentLanguage == 'ru') {
+                    currentDate = 'Четверг';
+                }
+
                 break;
             case 5:
-                currentDate = 'Friday';
+                if (currentLanguage == 'en') {
+                    currentDate = 'Friday';
+                } else if (currentLanguage == 'ru') {
+                    currentDate = 'Пятница';
+                }
+
                 break;
             case 6:
-                currentDate = 'Saturday';
+                if (currentLanguage == 'en') {
+                    currentDate = 'Saturday';
+                } else if (currentLanguage == 'ru') {
+                    currentDate = 'Суббота';
+                }
+
                 break;
             case 0:
-                currentDate = 'Sunday';
-                break;   
+                if (currentLanguage == 'en') {
+                    currentDate = 'Sunday';
+                } else if (currentLanguage == 'ru') {
+                    currentDate = 'Воскресенье';
+                }
+
+                break;
         }
     }
 
+    // if (weather && currentLanguage == 'ru') {
+    //     switch(item.weather[0].main) {
+
+    //     }
+    // }
+
+
+
+
+
     const onChangeCity = (e) => {
-        currentCity = e.currentTarget.value
+        currentCity = e.currentTarget.value;
+    }
+
+    const onChangeLanguage = (e) => {
+        // currentLanguage = e.currentTarget.value;
+        setCurrentLanguage(e.currentTarget.value);
+
     }
 
     return (
@@ -84,27 +161,41 @@ const Main = (props) => {
             <header className="content__header">
                 <div className="content__logo">
                     <img src={header_logo} width='50px' alt="" />
-                    <span>Weather App</span> 
+                    <span>Weather App</span>
                 </div>
+
                 <div className='content__search-block'>
-                    <input className='content__search' placeholder='City name' type="search" name="" id="" onChange={onChangeCity} />
-                    <button className='content__search-btn' onClick={searchCity}>Search</button>
+                    <div className='content__language'>
+                        <select onChange={onChangeLanguage}>
+                            <option value="en">en</option>
+                            <option value="ru">ru</option>
+                        </select>
+                    </div>
+                    <input className='content__search' placeholder={currentLanguage == 'en'
+                        ? 'City name'
+                        : 'Название города'} type="search" onChange={onChangeCity} />
+                    <button className='content__search-btn' onClick={searchCity}>
+                        {currentLanguage == 'en'
+                            ? 'Search'
+                            : 'Поиск'}
+                    </button>
                 </div>
             </header>
             <div className="content__row content__current">
                 {console.log(weather)}
                 {console.log(currentWeather)}
-     
                 <div className="content__col content__col-medium">
-                <div className='content__currentDate'>{currentDate}</div>
+                    <div className='content__currentDate'>{currentDate}</div>
                     <div className='content__row-default'>
-                        
+
                         <div className="content__col-default">
-                            <h1 className='content__main-title'>{currentWeather.data.weather[0].main}</h1>
+                            <h1 className='content__main-title'>
+                                {weather[0].weather[0].main}
+                            </h1>
                             <div className='content__currentWeather'>
                                 {currentWeather.data.main.temp}°
                             </div>
-                            
+
                         </div>
                         <div className="content__col-default">
                             <img src={`http://openweathermap.org/img/wn/${currentWeather.data.weather[0].icon}@2x.png`} alt="" />
@@ -119,39 +210,61 @@ const Main = (props) => {
                     <ul>
                         <li>
                             <img src={temp_feels_like} width='16' alt="" />
-                            <div>Feels like: <span>{currentWeather.data.main.feels_like}°</span> </div>        
+                            <div>{currentLanguage == 'en'
+                                ? 'Feels like'
+                                : 'Ощущается как'}: <span>{currentWeather.data.main.feels_like}°</span>
+                            </div>
                         </li>
-                        <li>
+                        {/* <li>
                             <img src={temp_max} width='16' alt="" />
-                            <div>Max temperature: <span>{currentWeather.data.main.temp_max}°</span>  </div>
-                                   
+                            <div>{currentLanguage == 'en'
+                                ? 'Max temperature'
+                                : 'Максимальная температура'}: <span>{currentWeather.data.main.temp_max}°</span>
+                            </div>
                         </li>
                         <li>
                             <img src={temp_min} width='16' alt="" />
-                            <div>Min temperature: <span>{currentWeather.data.main.temp_min}°</span> </div>
-                             
-                        </li>
+                            <div>{currentLanguage == 'en'
+                                ? 'Min temperature'
+                                : 'Минимальная температура'}: <span>{currentWeather.data.main.temp_min}°</span>
+                            </div>
+                        </li> */}
                         <li>
                             <img src={humidity} width='16' alt="" />
-                            <div>Humidity: <span>{currentWeather.data.main.humidity}%</span></div>
-                             
+                            <div>{currentLanguage == 'en'
+                                ? 'Humidity'
+                                : 'Влажность'}: <span>{currentWeather.data.main.humidity}%</span>
+                            </div>
                         </li>
                         <li>
                             <img src={temp_feels_like} width='16' alt="" />
-                            <div>Preassure: <span>{currentWeather.data.main.pressure} hPa</span> </div>
-                             
+                            <div>{currentLanguage == 'en'
+                                ? 'Preassure'
+                                : 'Давление'}: <span>{currentWeather.data.main.pressure} {currentLanguage == 'en'
+                                    ? 'chPa'
+                                    : 'гПа'}</span>
+                            </div>
                         </li>
                         <li>
                             <img src={visibility} width='16' alt="" />
-                            <div>Visibility: <span>{currentWeather.data.visibility} m</span></div>
-                             
+                            <div>{currentLanguage == 'en'
+                                ? 'Visibility'
+                                : 'Видимость'}: <span>{currentWeather.data.visibility} {currentLanguage == 'en'
+                                    ? 'm'
+                                    : 'м'}</span>
+                            </div>
                         </li>
                     </ul>
                 </div>
             </div>
             <div className="content__row content__forecast">
                 {weather.map((item, index) => {
-                    return <WeatherBlock id={index} title={item.weather[0].main} icon={item.weather[0].icon} weather={weather} />
+                    return <WeatherBlock
+                        id={index}
+                        title={item.weather[0].main}
+                        icon={item.weather[0].icon}
+                        weather={weather}
+                        currentLanguage={currentLanguage} />
                 })}
             </div>
         </div>
--- src/components/WeatherBlock/WeatherBlock.jsx
+++ src/components/WeatherBlock/WeatherBlock.jsx
@@ -5,69 +5,119 @@ const WeatherBlock = ({
     id,
     title,
     icon,
-    weather
+    weather,
+    currentLanguage
 }) => {
 
     const [isFull, changeList] = useState(false);
     let wd;
-   
 
     switch (new Date(weather[id].dt * 1000).getDay()) {
         case 1:
-            wd = 'Mon';
+            if (currentLanguage == 'ru') {
+                wd = 'Пн'
+            } else {
+                wd = 'Mon';
+            }
             break;
         case 2:
-            wd = 'Tue';
+            if (currentLanguage == 'ru') {
+                wd = 'Вт'
+            } else {
+                wd = 'Tue';
+            }
             break;
         case 3:
-            wd = 'Wed';
+            if (currentLanguage == 'ru') {
+                wd = 'Ср'
+            } else {
+                wd = 'Wed';
+            }
             break;
         case 4:
-            wd = 'Thu';
+            if (currentLanguage == 'ru') {
+                wd = 'Чт'
+            } else {
+                wd = 'Thu';
+            }
             break;
         case 5:
-            wd = 'Fri';
+            if (currentLanguage == 'ru') {
+                wd = 'Пт'
+            } else {
+                wd = 'Fri';
+            }
             break;
         case 6:
-            wd = 'Sat';
+            if (currentLanguage == 'ru') {
+                wd = 'Сб'
+            } else {
+                wd = 'Sat';
+            }
             break;
         case 0:
-            wd = 'Sun';
-            break;   
+            if (currentLanguage == 'ru') {
+                wd = 'Вс'
+            } else {
+                wd = 'Sun';
+            }
+            break;
     }
 
     return (
         <div className='weatherBlock__block'>
             <div className='weatherBlock__date'>{wd}</div>
             <div className='weatherBlock__title'>
-                
+
                 {title}
                 <img src={`http://openweathermap.org/img/wn/${icon}.png`} alt="" />
             </div>
             <div className="weatherBlock__content">
                 <h2 className='weatherBlock__description'>{weather[id].weather[0].description}</h2>
                 <ul className='weatherBlock__shortList'>
-                    <li>Morning: <span>{weather[id].temp.morn}°</span> </li>
-                    <li>Day: <span>{weather[id].temp.day}°</span> </li>
-                    <li>Evening: <span>{weather[id].temp.eve}°</span> </li>
-                    <li>Night: <span>{weather[id].temp.night}°</span> </li>
+                    <li>{currentLanguage == 'en'
+                        ? 'Morning'
+                        : 'Утро'}: <span>{weather[id].temp.morn}°</span> </li>
+                    <li>{currentLanguage == 'en'
+                        ? 'Day'
+                        : 'День'}: <span>{weather[id].temp.day}°</span> </li>
+                    <li>{currentLanguage == 'en'
+                        ? 'Evening'
+                        : 'Вечер'}: <span>{weather[id].temp.eve}°</span> </li>
+                    <li>{currentLanguage == 'en'
+                        ? 'Night'
+                        : 'Ночь'}: <span>{weather[id].temp.night}°</span> </li>
                 </ul>
-                {!isFull 
-                ? null 
-                : <ul className='weatherBlock__fullList'>
-                    <li>Max: <span>{weather[id].temp.max}°</span> </li>
-                    <li>Min: <span>{weather[id].temp.min}°</span> </li>
-                    <li>Humidity: <span>{weather[id].humidity}%</span> </li>
-                    <li>Pressure: <span>{weather[id].pressure} hPa</span> </li>
-                </ul>
+                {!isFull
+                    ? null
+                    : <ul className='weatherBlock__fullList'>
+                        <li>{currentLanguage == 'en'
+                            ? 'Max'
+                            : 'Максимум'}: <span>{weather[id].temp.max}°</span> </li>
+                        <li>{currentLanguage == 'en'
+                            ? 'Min'
+                            : 'Минимум'}: <span>{weather[id].temp.min}°</span> </li>
+                        <li>{currentLanguage == 'en'
+                            ? 'Humidity'
+                            : 'Влажность'}: <span>{weather[id].humidity}%</span> </li>
+                        <li>{currentLanguage == 'en'
+                            ? 'Pressure'
+                            : 'Давление'}: <span>{weather[id].pressure} {currentLanguage == 'en'
+                                ? 'hPa'
+                                : 'гПа'}</span> </li>
+                    </ul>
                 }
-            
+
             </div>
             {
-                    !isFull 
-                    ? <button className='weatherBlock__btn' onClick={() => changeList(true)}>show more</button> 
-                    : <button className='weatherBlock__btn' onClick={() => changeList(false)}>close</button>
-                }
+                !isFull
+                    ? <button className='weatherBlock__btn' onClick={() => changeList(true)}>{currentLanguage == 'en'
+                        ? 'show more'
+                        : 'показать еще'}</button>
+                    : <button className='weatherBlock__btn' onClick={() => changeList(false)}>{currentLanguage == 'en'
+                        ? 'close'
+                        : 'закрыть'}</button>
+            }
         </div>
     )
 }
